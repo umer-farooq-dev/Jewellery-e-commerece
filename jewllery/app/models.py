c9adf888e352dd570873b3b4b1d1aa6b37a15a5a
Umer Malik <ibsoft0786@gmail.com>
2022-04-03 18:06:25 +0500

    Initial Commit

---
+++ jewllery/app/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    locality = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    zipcode = models.IntegerField()
+
+
+def __str__(self):
+    return str(self.id)
+
+
+CATEGORY_CHOICES = (
+    ('R', 'Ring'),
+    ('B', 'Bracelet'),
+    ('N', 'Necklace'),
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discount_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='productimg')
+
+
+def __str__(self):
+    return str(self.id)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+def __str__(self):
+    return str(self.id)
+
+
+STATUS_CHOICES =(
+    ('Accepted', 'Accepted'),
+    ('Packed', 'Packed'),
+    ('On The way', 'On The way'),
+    ('Delivered', 'Delivered'),
+    ('Cancel', 'Cancel'),
+)
+
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
