f9566674980e0c0fcd4308103043e25339d98b71
Umer Malik <ibsoft0786@gmail.com>
2022-04-04 17:17:57 +0500

    Product Details

--- jewllery/app/urls.py
+++ jewllery/app/urls.py
@@ -6,9 +6,8 @@ from django.contrib.auth import views as auth_views
 from .forms import LoginForm, MyPasswordChangeForm
 
 urlpatterns = [
-                  # path('', views.home),
                   path('', views.ProductView.as_view(), name="home"),
-                  path('product-detail/', views.product_detail, name='product-detail'),
+                  path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
                   path('cart/', views.add_to_cart, name='add-to-cart'),
                   path('buy/', views.buy_now, name='buy-now'),
                   path('profile/', views.profile, name='profile'),
